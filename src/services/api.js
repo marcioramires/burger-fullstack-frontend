27c4b87d2c4db2906d3afc453ec06ccbab80ad9f
marcioramires <marcioramiresgt@gmail.com>
2023-01-13 23:53:10 -0300

    build

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: 'http://apiburger'
+    baseURL: 'http://localhost/api'
 })
 
 api.interceptors.request.use(async config => {
